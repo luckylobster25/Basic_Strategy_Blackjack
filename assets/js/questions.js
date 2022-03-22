2b411ed996b1ce7e46b07a3ec08f9bfd14c6659a
Chue Chang <chuechang1996@outlook.com>
2022-03-22 03:06:58 -0500

    adding files

---
+++ assets/js/questions.js
@@ -0,0 +1,104 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "Dealer showing a 2, player have 12",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "hit"
+  },
+  {
+    title: "Dealer showing a 10, player have 11",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "double down"
+  },
+  {
+    title: "Dealer showing a 3, player have 13",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "stand"
+  },
+  {
+    title: "Dealer showing an A, player have 11",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "double down"
+  },
+  {
+    title: "Dealer showing a 6, player have soft 19 (A8)",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "double down"
+  },
+  {
+    title: "Dealer showing a 4, player have a pair of 4",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "hit"
+  },
+  {
+    title: "Dealer showing 8, player have soft 18 (A7)",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "stand"
+  },
+  {
+    title: "Dealer showing a 3, player have a pair of 7",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "split"
+  },
+  {
+    title: "Dealer showing a 9, player have 10",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "double down"
+  },
+  {
+    title: "Dealer showing a 2, player have 13",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "stand"
+  },
+  {
+    title: "Dealer showing a 7, player have a pair of 9",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "stand"
+  },
+  {
+    title: "Dealer showing a 2, player have soft 17 (A6)",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "hit"
+  },
+  {
+    title: "Dealer showing a 7, player have a pair of 4",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "hit"
+  },
+  {
+    title: "Dealer showing a 5, player have a pair of 4",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "split"
+  },
+  {
+    title: "Dealer showing a 6, player have 8",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "hit"
+  },
+  {
+    title: "Dealer showing an A, player have a pair of 8",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "split"
+  },
+  {
+    title: "Dealer showing a 10, player have 18",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "hit"
+  },
+  {
+    title: "Dealer showing a 4, player have a soft 15 (A4)",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "double down"
+  },
+  {
+    title: "Dealer showing a 8, player have a pair of 7",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "stand"
+  },
+  {
+    title: "Dealer showing a 2, player have a pair of 4",
+    choices: ["hit", "stand", "double down", "split"],
+    answer: "hit"
+  },
+];
+var chue = "testing"
